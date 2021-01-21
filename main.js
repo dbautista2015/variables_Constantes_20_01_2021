c5f9e7fa1a20a22e74fc36cee5c71282f98b8df3
Diana Patricia Bautista Otalora <diana.bautista.otalora@gmail.com>
2021-01-20 21:08:42 -0500

    commit inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+var nombre="Diana";
+let edad=37;
+var profesor= false,
+lista_cursos=["Javascript", "PHP"]
+var informacion_personal={
+    id:14523697,
+    direccion:"calle 31"
+}
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacion_personal);
+const cedula = 1425
+console.warn(cedula);
